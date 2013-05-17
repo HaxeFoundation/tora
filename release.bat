9a19c2d58ac81d7b99fe03df679bd56f5ef1093a
Nicolas Cannasse <ncannasse@gmail.com>
2013-05-17 08:56:17 +0000

    move sources to src, switch to new haxelib

--- release.bat
+++ release.bat
@@ -3,10 +3,10 @@ haxe tora.hxml
 rm -rf release
 mkdir release
 mkdir release\tora
-cp haxelib.xml *.hx tora.hxml CHANGES.txt release
+cp haxelib.json tora.hxml CHANGES.txt release
 cp tora/*.hx release/tora
 cp tora.n release/run.n
-7z a -tzip release.zip release
-rm -rf release
-haxelib submit release.zip
+#7z a -tzip release.zip release
+#rm -rf release
+#haxelib submit release.zip
 pause
